ff23b5ce34f90db62b042ab897df756f17db12b2
winsonlow <winsonjjjj@gmail.com>
2022-03-21 20:40:21 +0800

    add

--- week02.js
+++ week02.js
@@ -17,6 +17,9 @@ client.db('sample_training').listCollections().toArray().then(result=>{
   console.log(result);
 })
 
+client.db('sample_training').collection('zips').find({'state':'NY'}).toArray().then(result=>{
+  console.log(result);
+})
 
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
