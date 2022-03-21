4745f26c208d046e2902ef8b41ab4fa9e9d2e5d8
winsonlow <winsonjjjj@gmail.com>
2022-03-21 20:39:15 +0800

    .

--- week02.js
+++ week02.js
@@ -17,9 +17,7 @@ client.db('sample_training').listCollections().toArray().then(result=>{
   console.log(result);
 })
 
-client.db('sample_training').collection('zips').find({'state':'NY'}).toArray().then(result=>{
-  console.log(result);
-})
+
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   //client.close();
